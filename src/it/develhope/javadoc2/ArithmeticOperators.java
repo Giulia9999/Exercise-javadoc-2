9045bf5231fbbb2fe02870f406789429220893f1
Giulia <giuliac35@gmail.com>
2023-01-19 18:38:40 +0100

    Define 4 methods on ArithmeticOperators class

--- src/it/develhope/javadoc2/ArithmeticOperators.java
+++ src/it/develhope/javadoc2/ArithmeticOperators.java
@@ -1,4 +1,20 @@
 package it.develhope.javadoc2;
 
 public class ArithmeticOperators {
+    public int sum(int sum1, int sum2){
+        int sum = sum1 + sum2;
+        return sum;
+    }
+    public int sub(int sub1, int sub2){
+        int sub = sub1 - sub2;
+        return sub;
+    }
+    public int mul(int mul1, int mul2){
+        int mul = mul1 * mul2;
+        return mul;
+    }
+    public int div(int div1, int div2){
+        int div = div1/div2;
+        return div;
+    }
 }
