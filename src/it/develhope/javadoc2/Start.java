446f1b1f34eb2a0137f883ba4fcb1a508a4f7449
Giulia <giuliac35@gmail.com>
2023-01-19 18:50:22 +0100

    Invoked ArithmeticOperators method in main method

--- src/it/develhope/javadoc2/Start.java
+++ src/it/develhope/javadoc2/Start.java
@@ -2,6 +2,10 @@ package it.develhope.javadoc2;
 
 public class Start {
     public static void main(String[] args) {
-
+        ArithmeticOperators arithmeticOperators = new ArithmeticOperators();
+        arithmeticOperators.sum(5,6);
+        arithmeticOperators.sub(10,5);
+        arithmeticOperators.mul(4,5);
+        arithmeticOperators.div(20,5);
     }
 }
