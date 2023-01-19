caaa580a55fc1f8f00307c0c34724ec45dbb4a45
Giulia <giuliac35@gmail.com>
2023-01-19 18:56:38 +0100

    Added javadoc comments on all the methods

--- src/it/develhope/javadoc2/ArithmeticOperators.java
+++ src/it/develhope/javadoc2/ArithmeticOperators.java
@@ -5,18 +5,45 @@ package it.develhope.javadoc2;
  * @author Giulia
  */
 public class ArithmeticOperators {
+    /**
+     * This method calculate the sum of the 2 int numbers
+     * @param sum1 The first int type number
+     * @param sum2 The second int type number
+     * @return the sum of the 2 int numbers
+     */
     public int sum(int sum1, int sum2){
         int sum = sum1 + sum2;
         return sum;
     }
+
+    /**
+     * This method calculate the subtraction of the 2 int numbers
+     * @param sub1 The first int type number
+     * @param sub2 The second int type number
+     * @return the subtraction of the 2 int numbers
+     */
     public int sub(int sub1, int sub2){
         int sub = sub1 - sub2;
         return sub;
     }
+
+    /**
+     * This method calculate the product of the 2 int numbers
+     * @param mul1 The first int type number
+     * @param mul2 The second int type number
+     * @return the product of the 2 int numbers
+     */
     public int mul(int mul1, int mul2){
         int mul = mul1 * mul2;
         return mul;
     }
+
+    /**
+     * This method calculate the division of the 2 int numbers
+     * @param div1 The first int type number
+     * @param div2 The second int type number
+     * @return the division of the 2 int numbers
+     */
     public int div(int div1, int div2){
         int div = div1/div2;
         return div;
--- src/it/develhope/javadoc2/Start.java
+++ src/it/develhope/javadoc2/Start.java
@@ -5,6 +5,10 @@ package it.develhope.javadoc2;
  * @author Giulia
  */
 public class Start {
+    /**
+     * This is the main method of the program
+     * @param args it takes arguments of String type and store it in a String array
+     */
     public static void main(String[] args) {
         ArithmeticOperators arithmeticOperators = new ArithmeticOperators();
         arithmeticOperators.sum(5,6);
